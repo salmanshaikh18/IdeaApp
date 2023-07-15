250f45b49d322e6de7ca0b5a23eeac754b78b99c
salmanshaikh18 <salmanshaikh18786@gmail.com>
2023-07-15 15:59:04 +0530

    database connection

--- Server.js
+++ Server.js
@@ -1,8 +1,34 @@
 const express = require('express'); 
-const serverConfig = require('./configs/server.config')
+const serverConfig = require('./configs/server.config');
+const mongoose = require('mongoose');
+const dbConfig = require('./configs/db.config');
+const userModel = require('./models/user.model')
+
+
 const app = express(); // app is a central controller in this server
+
+/**
+ * Logic to connect to MongoDB and create an ADMIN user
+ * Need to have the mongodb up and running in our local machine
+ */
+mongoose.connect(dbConfig.DB_URL); 
+const db = mongoose.connection;
+
+db.on("eroor", () => {
+    console.log("Error while connecting to DB");
+});
+
+db.once("open", () => {
+    console.log("Dadabae is connected");
+
+    init();
+})
+
+
+
+
 
 // Starting the Server
 app.listen(serverConfig.PORT, () => { 
-    console.log(`Server Started on the port number ${serverConfig.PORT}:)`)
+    console.log(`Server Started on the port number ${serverConfig.PORT}:)`);
 })
---
+++ models/user.model.js
@@ -0,0 +1,41 @@
+/**
+ * This will hold the schema for the user.
+ * It explains the different fields of use and how it will be stored in the mongodb
+ */
+
+const mongoose = require('mongoose') // require function is used to add module
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    }, 
+    userId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        minLength: true,
+        lowercase: true
+    },
+    userType: {
+        type: String,
+        required: true,
+        default: "CUSTOMER",
+        enum: ["CUTOMER", "ADMIN"] 
+    }
+}, {timestamp: true});
+
+/**
+ * Define the collection name where it will be stored
+ */
+
+module.exports = mongoose.model("User", userSchema)
